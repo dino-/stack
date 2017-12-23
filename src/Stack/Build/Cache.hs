dino-/stack

commit 99911566a0ac41203d8130bc3ae5fa724dd32128

Include full package location info in precompiled cache key #3346

src/Stack/Build/Cache.hs
@@ -43,10 +43,12 @@ import qualified Data.Set as Set
 import qualified Data.Store as Store
 import           Data.Store.VersionTagged
 import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
 import           Path
 import           Path.IO
 import           Stack.Constants.Config
 import           Stack.Types.Build
+import           Stack.Types.BuildPlan
 import           Stack.Types.Compiler
 import           Stack.Types.Config
 import           Stack.Types.GhcPkgId
@@ -226,49 +228,70 @@ checkTestSuccess dir =
 -- We only pay attention to non-directory options. We don't want to avoid a
 -- cache hit just because it was installed in a different directory.
 precompiledCacheFile :: (MonadThrow m, MonadReader env m, HasEnvConfig env, MonadLogger m)
-                     => PackageIdentifier
+                     => PackageLocationIndex FilePath
                      -> ConfigureOpts
                      -> Set GhcPkgId -- ^ dependencies
-                     -> m (Path Abs File)
-precompiledCacheFile pkgident copts installedPackageIDs = do
-    ec <- view envConfigL
+                     -> m (Maybe (Path Abs File))
+precompiledCacheFile loc copts installedPackageIDs = do
+  ec <- view envConfigL
 
-    compiler <- view actualCompilerVersionL >>= parseRelDir . compilerVersionString
-    cabal <- view cabalVersionL >>= parseRelDir . versionString
-    pkg <- parseRelDir $ packageIdentifierString pkgident
+  compiler <- view actualCompilerVersionL >>= parseRelDir . compilerVersionString
+  cabal <- view cabalVersionL >>= parseRelDir . versionString
+  let mpkgRaw =
+        -- The goal here is to come up with a string representing the
+        -- package location which is unique. For archives and repos,
+        -- we rely upon cryptographic hashes paired with
+        -- subdirectories to identify this specific package version.
+        case loc of
+          PLIndex pir -> Just $ packageIdentifierRevisionString pir
+          PLOther other -> case other of
+            PLFilePath _ -> assert False Nothing -- no PLFilePaths should end up in a snapshot
+            PLArchive a -> fmap
+              (\h -> T.unpack (staticSHA256ToText h) ++ archiveSubdirs a)
+              (archiveHash a)
+            PLRepo r -> Just $ T.unpack (repoCommit r) ++ repoSubdirs r
+
+  forM mpkgRaw $ \pkgRaw -> do
+    pkg <-
+      case parseRelDir pkgRaw of
+        Just x -> return x
+        Nothing -> parseRelDir
+                 $ T.unpack
+                 $ TE.decodeUtf8
+                 $ B64URL.encode
+                 $ TE.encodeUtf8
+                 $ T.pack pkgRaw
     platformRelDir <- platformGhcRelDir
-
-    let input = (coNoDirs copts, installedPackageIDs)
 
     -- In Cabal versions 1.22 and later, the configure options contain the
     -- installed package IDs, which is what we need for a unique hash.
     -- Unfortunately, earlier Cabals don't have the information, so we must
     -- supplement it with the installed package IDs directly.
     -- See issue: https://github.com/commercialhaskell/stack/issues/1103
-    let hashToPath hash = do
-            hashPath <- parseRelFile $ S8.unpack hash
-            return $ view stackRootL ec
-                 </> $(mkRelDir "precompiled")
-                 </> platformRelDir
-                 </> compiler
-                 </> cabal
-                 </> pkg
-                 </> hashPath
+    let input = (coNoDirs copts, installedPackageIDs)
+    hashPath <- parseRelFile $ S8.unpack $ B64URL.encode
+              $ Mem.convert $ hashWith SHA256 $ Store.encode input
 
-    newPath <- hashToPath $ B64URL.encode $ Mem.convert $ hashWith SHA256 $ Store.encode input
-    return newPath
+    return $ view stackRootL ec
+         </> $(mkRelDir "precompiled")
+         </> platformRelDir
+         </> compiler
+         </> cabal
+         </> pkg
+         </> hashPath
 
 -- | Write out information about a newly built package
 writePrecompiledCache :: (MonadThrow m, MonadReader env m, HasEnvConfig env, MonadIO m, MonadLogger m)
                       => BaseConfigOpts
-                      -> PackageIdentifier
+                      -> PackageLocationIndex FilePath
                       -> ConfigureOpts
                       -> Set GhcPkgId -- ^ dependencies
                       -> Installed -- ^ library
                       -> Set Text -- ^ executables
                       -> m ()
-writePrecompiledCache baseConfigOpts pkgident copts depIDs mghcPkgId exes = do
-    file <- precompiledCacheFile pkgident copts depIDs
+writePrecompiledCache baseConfigOpts loc copts depIDs mghcPkgId exes = do
+  mfile <- precompiledCacheFile loc copts depIDs
+  forM_ mfile $ \file -> do
     ensureDir (parent file)
     ec <- view envConfigL
     let stackRootRelative = makeRelative (view stackRootL ec)
@@ -291,10 +314,10 @@ writePrecompiledCache baseConfigOpts pkgident copts depIDs mghcPkgId exes = do
 -- | Check the cache for a precompiled package matching the given
 -- configuration.
 readPrecompiledCache :: (MonadThrow m, MonadReader env m, HasEnvConfig env, MonadUnliftIO m, MonadLogger m)
-                     => PackageIdentifier -- ^ target package
+                     => PackageLocationIndex FilePath -- ^ target package
                      -> ConfigureOpts
                      -> Set GhcPkgId -- ^ dependencies
                      -> m (Maybe PrecompiledCache)
-readPrecompiledCache pkgident copts depIDs = do
-    file <- precompiledCacheFile pkgident copts depIDs
-    $(versionedDecodeFile precompiledCacheVC) file
+readPrecompiledCache loc copts depIDs =
+    precompiledCacheFile loc copts depIDs >>=
+    maybe (return Nothing) $(versionedDecodeFile precompiledCacheVC)

src/Stack/Build/Execute.hs
@@ -1214,9 +1214,13 @@ singleBuild runInBase ac@ActionContext {..} ee@ExecuteEnv {..} task@Task {..} in
     getPrecompiled cache =
         case taskLocation task of
             Snap | not shouldHaddockPackage' -> do
-                mpc <- readPrecompiledCache taskProvides
-                    (configCacheOpts cache)
-                    (configCacheDeps cache)
+                mpc <-
+                  case taskType of
+                    TTUpstream _ _ loc -> readPrecompiledCache
+                      loc
+                      (configCacheOpts cache)
+                      (configCacheDeps cache)
+                    _ -> return Nothing
                 case mpc of
                     Nothing -> return Nothing
                     -- Only pay attention to precompiled caches that refer to packages within
@@ -1443,12 +1447,15 @@ singleBuild runInBase ac@ActionContext {..} ee@ExecuteEnv {..} task@Task {..} in
                 markExeInstalled (taskLocation task) taskProvides -- TODO unify somehow with writeFlagCache?
                 return $ Executable ident
 
-        case taskLocation task of
-            Snap -> writePrecompiledCache eeBaseConfigOpts taskProvides
+        case (taskLocation task, taskType) of
+            (Snap, TTUpstream _ _ loc) ->
+              writePrecompiledCache
+                eeBaseConfigOpts
+                loc
                 (configCacheOpts cache)
                 (configCacheDeps cache)
                 mpkgid (packageExes package)
-            Local -> return ()
+            _ -> return ()
 
         case taskType of
             -- For upstream packages from a package index, pkgDir is in the tmp

commit 880efce636227bba012e6ebdc9a2155c4616fd9c

Avoid MAX_PATH for precompiled cache #3649
Regression was in 99911566a0ac41203d8130bc3ae5fa724dd32128

src/Stack/Build/Cache.hs
@@ -32,6 +32,7 @@ module Stack.Build.Cache
     , BuildCache(..)
     ) where
 
+import           Stack.Constants
 import           Stack.Prelude
 import           Crypto.Hash (hashWith, SHA256(..))
 import           Control.Monad.Trans.Maybe
@@ -254,6 +255,14 @@ precompiledCacheFile loc copts installedPackageIDs = do
             PLRepo r -> Just $ T.unpack (repoCommit r) ++ repoSubdirs r
 
   forM mpkgRaw $ \pkgRaw -> do
+    platformRelDir <- platformGhcRelDir
+    let precompiledDir =
+              view stackRootL ec
+          </> $(mkRelDir "precompiled")
+          </> platformRelDir
+          </> compiler
+          </> cabal
+
     pkg <-
       case parseRelDir pkgRaw of
         Just x -> return x
@@ -263,7 +272,6 @@ precompiledCacheFile loc copts installedPackageIDs = do
                  $ B64URL.encode
                  $ TE.encodeUtf8
                  $ T.pack pkgRaw
-    platformRelDir <- platformGhcRelDir
 
     -- In Cabal versions 1.22 and later, the configure options contain the
     -- installed package IDs, which is what we need for a unique hash.
@@ -274,13 +282,19 @@ precompiledCacheFile loc copts installedPackageIDs = do
     hashPath <- parseRelFile $ S8.unpack $ B64URL.encode
               $ Mem.convert $ hashWith SHA256 $ Store.encode input
 
-    return $ view stackRootL ec
-         </> $(mkRelDir "precompiled")
-         </> platformRelDir
-         </> compiler
-         </> cabal
-         </> pkg
-         </> hashPath
+    let longPath = precompiledDir </> pkg </> hashPath
+
+    -- See #3649 - shorten the paths on windows if MAX_PATH will be
+    -- violated. Doing this only when necessary allows use of existing
+    -- precompiled packages.
+    case maxPathLength of
+      Nothing -> return longPath
+      Just maxPath
+        | length (toFilePath longPath) > maxPath -> do
+            shortPkg <- shaPath pkg
+            shortHash <- shaPath hashPath
+            return $ precompiledDir </> shortPkg </> shortHash
+        | otherwise -> return longPath
 
 -- | Write out information about a newly built package
 writePrecompiledCache :: (MonadThrow m, MonadReader env m, HasEnvConfig env, MonadIO m, MonadLogger m)

src/Stack/Constants.hs
@@ -1,6 +1,7 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 -- | Constants used throughout the project.
 
@@ -32,6 +33,7 @@ module Stack.Constants
     ,minTerminalWidth
     ,maxTerminalWidth
     ,defaultTerminalWidth
+    ,maxPathLength
     )
     where
 
@@ -241,3 +243,12 @@ maxTerminalWidth = 200
 -- automatically detect it and when the user doesn't supply one.
 defaultTerminalWidth :: Int
 defaultTerminalWidth = 100
+
+-- | Maximum length to use in paths. Is only a 'Just' value on windows,
+-- corresponding to MAX_PATH.
+maxPathLength :: Maybe Int
+#ifdef mings32_HOST_OS
+maxPathLength = Just 260
+#else
+maxPathLength = Nothing
+#endif

src/Stack/Types/Config.hs
@@ -1,6 +1,6 @@
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DefaultSignatures #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DeriveFoldable #-}
@@ -9,17 +9,18 @@
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE StandaloneDeriving #-}
 
 -- | The Config type.
 
@@ -131,6 +132,7 @@ module Stack.Types.Config
   ,platformGhcRelDir
   ,platformGhcVerOnlyRelDir
   ,useShaPathOnWindows
+  ,shaPath
   ,workDirL
   -- * Command-specific types
   -- ** Eval
@@ -176,6 +178,7 @@ module Stack.Types.Config
   ) where
 
 import           Control.Monad.Writer (tell)
+import           Crypto.Hash (hashWith, SHA1(..))
 import           Stack.Prelude
 import           Data.Aeson.Extended
                  (ToJSON, toJSON, FromJSON, FromJSONKey (..), parseJSON, withText, object,
@@ -184,6 +187,7 @@ import           Data.Aeson.Extended
                   jsonSubWarningsT, jsonSubWarningsTT, WithJSONWarnings(..), noJSONWarnings,
                   FromJSONKeyFunction (FromJSONKeyTextParser))
 import           Data.Attoparsec.Args (parseArgs, EscapingMode (Escaping))
+import qualified Data.ByteArray.Encoding as Mem (convertToBase, Base(Base16))
 import qualified Data.ByteString.Char8 as S8
 import           Data.List (stripPrefix)
 import           Data.List.NonEmpty (NonEmpty)
@@ -230,11 +234,6 @@ import           System.Process.Read (EnvOverride, findExecutable)
 
 -- Re-exports
 import           Stack.Types.Config.Build as X
-
-#ifdef mingw32_HOST_OS
-import           Crypto.Hash (hashWith, SHA1(..))
-import qualified Data.ByteArray.Encoding as Mem (convertToBase, Base(Base16))
-#endif
 
 -- | The top-level Stackage configuration.
 data Config =
@@ -1372,10 +1371,26 @@ platformGhcVerOnlyRelDirStr = do
 useShaPathOnWindows :: MonadThrow m => Path Rel Dir -> m (Path Rel Dir)
 useShaPathOnWindows =
 #ifdef mingw32_HOST_OS
-    parseRelDir . S8.unpack . S8.take 8 . Mem.convertToBase Mem.Base16 . hashWith SHA1 . encodeUtf8 . T.pack . toFilePath
+    shaPath
 #else
     return
 #endif
+
+shaPath :: (IsPath Rel t, MonadThrow m) => Path Rel t -> m (Path Rel t)
+shaPath
+    = parsePath . S8.unpack . S8.take 8
+    . Mem.convertToBase Mem.Base16 . hashWith SHA1
+    . encodeUtf8 . T.pack . toFilePath
+
+-- TODO: Move something like this into the path package. Consider
+-- subsuming path-io's 'AnyPath'?
+class IsPath b t where
+  parsePath :: MonadThrow m => FilePath -> m (Path b t)
+
+instance IsPath Abs Dir where parsePath = parseAbsDir
+instance IsPath Rel Dir where parsePath = parseRelDir
+instance IsPath Abs File where parsePath = parseAbsFile
+instance IsPath Rel File where parsePath = parseRelFile
 
 compilerVersionDir :: (MonadThrow m, MonadReader env m, HasEnvConfig env) => m (Path Rel Dir)
 compilerVersionDir = do